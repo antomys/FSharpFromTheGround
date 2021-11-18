42f01610a4c6bfc4acb2b79351743dc0bf8f5cc4
ihor.volokhovych <Mieight11997733!>
2021-11-18 13:15:15 +0200

    If then else

---
+++ ControlFlow/ControlFlow/Program.fs
@@ -0,0 +1,10 @@
+let from whom =
+    $"from {whom}"
+
+[<EntryPoint>]
+let main argv =
+    let message = (from "F#") // Call the function
+    let person = if argv.Length > 0 then argv.[0] else ""
+        
+    printfn $"Hello world {message}, {person}"
+    0 // return an integer exit code

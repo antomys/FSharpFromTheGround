4142f5803171d515e69aade1c4fd67022bbd180e
ihor.volokhovych <Mieight11997733!>
2021-11-18 13:49:44 +0200

    Loops and iteration. For, Foreach and Array.Iter

--- ControlFlow/LoopsIteration/Program.fs
+++ ControlFlow/LoopsIteration/Program.fs
@@ -1,8 +1,26 @@
-
-[<EntryPoint>]
-let main argv =
+    // for construction in fsharp
+let forLoop (argv : string[])  =
     for i in 0..argv.Length-1 do
         let person = argv.[i]
         printfn $"{person}"
+        
+     // foreach loop
+let foreachLoop (argv : string[])  =
+     for variable in argv do
+        printfn $"{variable}"
+
+    // Array.iter f# approach
+let printOut stringInput =
+    printfn $"{stringInput}"
+    
+let arrayIter (argv : string[]) =
+    Array.iter printOut argv
+    
+
+[<EntryPoint>]
+let main argv =
+    forLoop(argv)
+    foreachLoop(argv)
+    arrayIter(argv)
     printfn "Hello f#"
     0

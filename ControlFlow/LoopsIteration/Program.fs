37f033226b1906ef47e943be208bb543ebd81696
ihor.volokhovych <Mieight11997733!>
2021-11-18 15:30:52 +0200

    removed brackets

--- ControlFlow/LoopsIteration/Program.fs
+++ ControlFlow/LoopsIteration/Program.fs
@@ -1,3 +1,4 @@
+open System;    
     // for construction in fsharp
 let forLoop (argv : string[])  =
     for i in 0..argv.Length-1 do
@@ -9,6 +10,11 @@ let foreachLoop (argv : string[])  =
      for variable in argv do
         printfn $"{variable}"
 
+    
+    // filter invalid input
+let isValid inputString =
+    String.IsNullOrWhiteSpace(inputString) |> not
+    
     // Array.iter f# approach
 let printOut stringInput =
     printfn $"{stringInput}"
@@ -18,11 +24,20 @@ let arrayIter (argv : string[]) =
     // (a func that takes one or more functions as arguments)
     Array.iter printOut argv
     
+    // Filtering firstly and then printing
+let arrayIterV2 (argv : string[]) =
+    // Higher order functions
+    // (a func that takes one or more functions as arguments)
+    argv
+    |> Array.filter isValid
+    |> Array.iter printOut
+    
 
 [<EntryPoint>]
 let main argv =
-    forLoop(argv)
-    foreachLoop(argv)
-    arrayIter(argv)
+    forLoop argv
+    foreachLoop argv
+    arrayIter argv
+    arrayIterV2 argv
     printfn "Hello f#"
     0

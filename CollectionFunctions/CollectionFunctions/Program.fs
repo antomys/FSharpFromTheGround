ba8d75fd83185c2c64a4025f74088bc385834f03
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-11-21 20:00:41 +0200

    Finished section 6, records

--- CollectionFunctions/CollectionFunctions/Program.fs
+++ CollectionFunctions/CollectionFunctions/Program.fs
@@ -1,81 +1,49 @@
-open System.IO
-open System
-let checkArgs (args:string[]) =
-    args.Length <= 0
+namespace CollectionFunctions
 
-let doesFileExist filePath =
-    let isExist = File.Exists filePath
-    
-    if isExist then
-        printfn $"File {filePath} exists! Proceeding."
-    else
-        printfn $"File {filePath} not found"
-    
-    isExist
+module Program =
+    open System.IO
+    let checkArgs (args:string[]) =
+        args.Length <= 0
 
-let getStudentsCount (studentFile:string[]) =
-    (studentFile |> Array.length)
-    
-let getMeanScore (studentRow: string) =
-    let studentElements = studentRow.Split("\t")
-    let omitStringArray = studentElements |> Array.skip 2
-    
-    let mean =
-        omitStringArray
-        |> Array.map float
-        |> Array.average
-    
-    let min =
-        omitStringArray
-        |> Array.minBy float
-    let max =
-        omitStringArray
-        |> Array.maxBy float
+    let doesFileExist filePath =
+        let isExist = File.Exists filePath
         
-    $"{studentElements.[0]}:{studentElements.[1]}:mean:{mean}:min:{min}:max:{max}"
-    
-let getMeanScoreV2 (studentRow: string) =
-    let studentElements = studentRow.Split("\t")
-    let mapped =
-        studentElements
-        |> Array.skip 2
-        |> Array.map float
-    let mean = mapped |> Array.average
-    let max = mapped |> Array.max
-    let min = mapped |> Array.min
-
-    $"{studentElements.[0]}:{studentElements.[1]}:mean:{mean}:min:{min}:max:{max}"
-    
-let getMeanScoreV3 (studentRow: string) =
-    let studentElements = studentRow.Split("\t")
-    let mapped =
-        studentElements
-        |> Array.skip 2
-        |> Array.map float
-    
-    let mean = mapped |> Array.average
-    let tupleStack = (Array.max mapped, Array.min mapped)
-
-    $"{studentElements.[0]}:{studentElements.[1]}:mean:{mean}:min:{fst tupleStack}:max:{snd tupleStack}"
-
-
-[<EntryPoint>]
-let main args =
-    if args |> checkArgs = false then
-        printfn($"Processing file {args.[0]}")
-        
-        if doesFileExist args.[0] then
-            let studentRows = args.[0]
-                              |> File.ReadAllLines
-                              |> Array.skip 1
-                              
-            printfn $"Student rows : {studentRows |> getStudentsCount}"
-            printfn $"{getMeanScore studentRows.[0]}"
-            printfn $"{getMeanScoreV2 studentRows.[0]}"
-            printfn $"{getMeanScoreV3 studentRows.[0]}"
-            0
+        if isExist then
+            printfn $"File {filePath} exists! Proceeding." 
         else
+            printfn $"File {filePath} not found"
+        
+        isExist
+
+    [<EntryPoint>]
+    let main args =
+        if args |> checkArgs = false then
+            printfn($"Processing file {args.[0]}")
+            
+            if doesFileExist args.[0] then
+
+                let _ = args.[0]
+                        |> File.ReadAllLines
+                        |> Array.skip 1
+                        |> Array.map Student.fromString
+                        |> Array.sortBy (fun student -> student.Name)
+                        |> Array.iter Student.printSummary
+                0
+            else
+                0
+        else
+            printf("No args are assigned. Exit.")
             0
-    else
-        printf("No args are assigned. Exit.")
-        0
+            
+            
+            
+                            // v1 version
+                (*let studentRows = args.[0]
+                                  |> File.ReadAllLines
+                                  |> Array.skip 1
+               
+                for studentRow in studentRows do
+                     studentRow
+                     |> Student.fromString
+                     |> Student.printSummary*)
+                     
---
+++ CollectionFunctions/CollectionFunctions/Student.fs
@@ -0,0 +1,93 @@
+﻿namespace CollectionFunctions
+
+open System.Collections.Generic
+
+type Student =
+    {
+        Name : string
+        StudentId : string
+        MeanScore : float
+        MinScore : float
+        MaxScore : float
+    }
+
+module Student =
+    
+    let printSummary (student : Student) =
+        printfn $"Student {student.Name}; Mean : {student.MeanScore}; Max : {student.MaxScore}; Min : {student.MinScore}"
+        
+    let getStudentsCount (studentFile:string[]) =
+        studentFile |> Array.length
+    
+    let getMeanScore (studentRow: string) =
+        let studentElements = studentRow.Split("\t")
+        let omitStringArray = studentElements |> Array.skip 2
+        
+        let mean =
+            omitStringArray
+            |> Array.map float
+            |> Array.average
+        
+        let min =
+            omitStringArray
+            |> Array.minBy float
+        let max =
+            omitStringArray
+            |> Array.maxBy float
+            
+        $"{studentElements.[0]}:{studentElements.[1]}:mean:{mean}:min:{min}:max:{max}"
+    
+    let getMeanScoreV2 (studentRow: string) =
+        let studentElements = studentRow.Split("\t")
+        let mapped =
+            studentElements
+            |> Array.skip 2
+            |> Array.map float
+        let mean = mapped |> Array.average
+        let max = mapped |> Array.max
+        let min = mapped |> Array.min
+
+        $"{studentElements.[0]}:{studentElements.[1]}:mean:{mean}:min:{min}:max:{max}"
+        
+        
+    let getMeanScoreV3 (studentRow: string) =
+        let studentElements = studentRow.Split("\t")
+        let mapped =
+            studentElements
+            |> Array.skip 2
+            |> Array.map float
+        
+        let mean = mapped |> Array.average
+        let tupleStack = (Array.max mapped, Array.min mapped)
+
+        $"{studentElements.[0]}:{studentElements.[1]}:mean:{mean}:min:{snd tupleStack}:max:{fst tupleStack}"
+
+    let fromString (inputString : string) =
+        let studentElements = inputString.Split("\t")
+        let mapped =
+            studentElements
+            |> Array.skip 2
+            |> Array.map float
+        
+        {
+            Name = studentElements.[0]
+            StudentId = studentElements.[1]
+            MeanScore = mapped |> Array.average
+            MaxScore = mapped |> Array.max
+            MinScore = mapped |> Array.min
+        }
+        
+        (*for studentString in inputString ->
+        (let studentElements = inputString.Split("\t")
+            let mapped =
+                studentElements
+                |> Array.skip 2
+                |> Array.map float
+            {
+                Name = studentElements.[0]
+                StudentId = studentElements.[1]
+                MeanScore = mapped |> Array.average
+                MaxScore = mapped |> Array.max
+                MinScore = mapped |> Array.min
+            })
+            ]*)

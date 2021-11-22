620a9510b7e618b92ad5f083308841422e7dd03f
Ihor Volokhovych <igorvolokhovych@gmail.com>
2021-11-23 01:08:12 +0200

    Completed 25. DUnions, 26 Matches

---
+++ CollectionFunctions/CollectionFunctionsAe/Program.fs
@@ -0,0 +1,32 @@
+open CollectionFunctions.Common
+
+module CollectionFunctionsAe =
+
+    [<EntryPoint>]
+    let main args =
+        if args.Length <= 0 then
+            printfn "Arguments are obsolete. Exit"
+            0
+        else
+            let fileLines =
+                args.[0]
+                |> FileService.tryOpenRead
+                |> FileService.filterNone
+            
+            if fileLines.Length = 0 then
+                printfn "Error reading. Exit"
+                0
+            else
+                let _ =fileLines
+                    |> Array.map StudentV2.fromStringV2
+                    |> Array.sortBy (fun student -> student.Name)
+                    |> Array.iter Summary.printSummaryV2
+                
+                printfn "--------------------------------"
+                
+                let _ =fileLines
+                    |> Array.map StudentV2.fromStringV3
+                    |> Array.sortBy (fun student -> student.Name)
+                    |> Array.iter Summary.printSummaryV2
+                0
+               
